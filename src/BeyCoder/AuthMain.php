ff9a802ebb36c27eb5e36b78b6644d508a4e9944
Beybut Abdulragimov <beybut2013@gmail.com>
2020-02-13 17:45:36 +0400

    initial

---
+++ src/BeyCoder/AuthMain.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace BeyCoder;
+
+use pocketmine\plugin\PluginBase;
+
+class AuthMain extends PluginBase
+{
+    /**
+     * @var ApiManager $api
+     */
+    private $api;
+
+    public function onEnable()
+    {
+        $this->getLogger()->info("Система авторизации включена!");
+
+        $this->api = $this->getServer()->getPluginManager()->getPlugin("cwApiPE");
+        if($this->getApi() == null)
+        {
+            $this->getLogger()->info("Плагин cwApiPE не найден! Установите и попробуйте запустить ещё раз");
+
+        }
+    }
+
+    /**
+     * @return ApiManager
+     */
+    public function getApi(): ApiManager
+    {
+        return $this->api;
+    }
+}
